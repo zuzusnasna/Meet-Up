85375de406bd562b6837cd1f0a6ec0a80fb306b8
jusanghyeok <jsmh991102@gmail.com>
2026-09-24 23:23:31 +0900

    fix: ignore unknown Kakao place fields

--- src/main/java/com/example/meetup/dto/KakaoPlaceDto.java
+++ src/main/java/com/example/meetup/dto/KakaoPlaceDto.java
@@ -1,9 +1,11 @@
 package com.example.meetup.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoPlaceDto {
 
     private String id;
