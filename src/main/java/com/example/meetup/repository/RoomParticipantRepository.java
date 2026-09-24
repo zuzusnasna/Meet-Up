42ad39d52c32eb21797b161cf7caec4a1f99021f
jusanghyeok <jsmh991102@gmail.com>
2026-09-25 01:10:27 +0900

    복합키에 맞게 모임 참여자 Repository 수정

--- src/main/java/com/example/meetup/repository/RoomParticipantRepository.java
+++ src/main/java/com/example/meetup/repository/RoomParticipantRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface RoomParticipantRepository extends JpaRepository<RoomParticipant, Long> {
+public interface RoomParticipantRepository
+        extends JpaRepository<RoomParticipant, RoomParticipant.RoomParticipantId> {
+
     List<RoomParticipant> findByRoomId(Long roomId);
+
     Optional<RoomParticipant> findByRoomIdAndUserId(Long roomId, Long userId);
 }
