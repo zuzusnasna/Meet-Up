c97914015656c12d6cc61f2963c8202d04caa91a
jusanghyeok <jsmh991102@gmail.com>
2026-09-25 01:06:20 +0900

    RoomController.java 구현

---
+++ src/main/java/com/example/meetup/controller/RoomController.java
@@ -0,0 +1,43 @@
+package com.example.meetup.controller;
+
+import com.example.meetup.entity.Room;
+import com.example.meetup.entity.RoomParticipant;
+import com.example.meetup.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public Room createRoom(
+            @RequestParam String roomName,
+            @RequestParam Long creatorId
+    ) {
+        return roomService.createRoom(roomName, creatorId);
+    }
+
+    @GetMapping("/{roomId}")
+    public Room findById(@PathVariable Long roomId) {
+        return roomService.findById(roomId);
+    }
+
+    @PostMapping("/{roomId}/participants")
+    public RoomParticipant joinRoom(
+            @PathVariable Long roomId,
+            @RequestParam Long userId
+    ) {
+        return roomService.joinRoom(roomId, userId);
+    }
+
+    @GetMapping("/{roomId}/participants")
+    public List<RoomParticipant> findParticipants(@PathVariable Long roomId) {
+        return roomService.findParticipants(roomId);
+    }
+}
