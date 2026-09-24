bfff2a9c400c65598b94f229cfde61085702f5a6
jusanghyeok <jsmh991102@gmail.com>
2026-09-25 00:57:29 +0900

    지도 메모 기능 백엔드 추가

---
+++ src/main/java/com/example/meetup/repository/MemoRepository.java
@@ -0,0 +1,11 @@
+package com.example.meetup.repository;
+
+import com.example.meetup.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+
+    List<Memo> findByRoomId(Long roomId);
+}
