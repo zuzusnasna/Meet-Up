7a78d9e0622b5e40ee885008bffad41fc91d5918
jusanghyeok <jsmh991102@gmail.com>
2026-09-24 23:24:23 +0900

    fix: ignore unknown Kakao response fields

--- src/main/java/com/example/meetup/dto/KakaoPlaceResponse.java
+++ src/main/java/com/example/meetup/dto/KakaoPlaceResponse.java
@@ -1,10 +1,12 @@
 package com.example.meetup.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class KakaoPlaceResponse {
 
     private List<KakaoPlaceDto> documents;
